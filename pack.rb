99f8a138de26d6c3b53d000f0bf603e7b1c2f283
Horimoto Yasuhiro <horimoto@clear-code.com>
2021-02-03 18:18:00 +0900

    Add process for download target packages

---
+++ pack.rb
@@ -0,0 +1,128 @@
+require 'rake'
+require 'zip'
+
+download_packages = [
+  "groonga-10.0.4-x64-vs2013-with-vcruntime.zip",
+  "groonga-10.0.4-x64-vs2015-with-vcruntime.zip",
+  "groonga-10.0.4-x64-vs2017-with-vcruntime.zip",
+  "groonga-10.0.4-x64-vs2013.zip",
+  "groonga-10.0.4-x64-vs2015.zip",
+  "groonga-10.0.4-x64-vs2017.zip",
+  "groonga-10.0.4-x64.zip",
+
+  "groonga-10.0.4-x86-vs2013-with-vcruntime.zip",
+  "groonga-10.0.4-x86-vs2015-with-vcruntime.zip",
+  "groonga-10.0.4-x86-vs2017-with-vcruntime.zip",
+  "groonga-10.0.4-x86-vs2013.zip",
+  "groonga-10.0.4-x86-vs2015.zip",
+  "groonga-10.0.4-x86-vs2017.zip",
+  "groonga-10.0.4-x86.zip",
+
+  "groonga-10.0.5-x64-vs2015-with-vcruntime.zip",
+  "groonga-10.0.5-x64-vs2017-with-vcruntime.zip",
+  "groonga-10.0.5-x64-vs2019-with-vcruntime.zip",
+  "groonga-10.0.5-x64-vs2015.zip",
+  "groonga-10.0.5-x64-vs2017.zip",
+  "groonga-10.0.5-x64-vs2019.zip",
+  "groonga-10.0.5-x64.zip",
+
+  "groonga-10.0.5-x86-vs2015-with-vcruntime.zip",
+  "groonga-10.0.5-x86-vs2017-with-vcruntime.zip",
+  "groonga-10.0.5-x86-vs2019-with-vcruntime.zip",
+  "groonga-10.0.5-x86-vs2015.zip",
+  "groonga-10.0.5-x86-vs2017.zip",
+  "groonga-10.0.5-x86-vs2019.zip",
+  "groonga-10.0.5-x86.zip",
+
+  "groonga-10.0.6-x64-vs2015-with-vcruntime.zip",
+  "groonga-10.0.6-x64-vs2017-with-vcruntime.zip",
+  "groonga-10.0.6-x64-vs2019-with-vcruntime.zip",
+  "groonga-10.0.6-x64-vs2015.zip",
+  "groonga-10.0.6-x64-vs2017.zip",
+  "groonga-10.0.6-x64-vs2019.zip",
+  "groonga-10.0.6-x64.zip",
+
+  "groonga-10.0.6-x86-vs2015-with-vcruntime.zip",
+  "groonga-10.0.6-x86-vs2017-with-vcruntime.zip",
+  "groonga-10.0.6-x86-vs2019-with-vcruntime.zip",
+  "groonga-10.0.6-x86-vs2015.zip",
+  "groonga-10.0.6-x86-vs2017.zip",
+  "groonga-10.0.6-x86-vs2019.zip",
+  "groonga-10.0.6-x86.zip",
+
+  "groonga-10.0.7-x64-vs2015-with-vcruntime.zip",
+  "groonga-10.0.7-x64-vs2017-with-vcruntime.zip",
+  "groonga-10.0.7-x64-vs2019-with-vcruntime.zip",
+  "groonga-10.0.7-x64-vs2015.zip",
+  "groonga-10.0.7-x64-vs2017.zip",
+  "groonga-10.0.7-x64-vs2019.zip",
+  "groonga-10.0.7-x64.zip",
+
+  "groonga-10.0.7-x86-vs2015-with-vcruntime.zip",
+  "groonga-10.0.7-x86-vs2017-with-vcruntime.zip",
+  "groonga-10.0.7-x86-vs2019-with-vcruntime.zip",
+  "groonga-10.0.7-x86-vs2015.zip",
+  "groonga-10.0.7-x86-vs2017.zip",
+  "groonga-10.0.7-x86-vs2019.zip",
+  "groonga-10.0.7-x86.zip",
+
+  "groonga-10.0.8-x64-vs2017-with-vcruntime.zip",
+  "groonga-10.0.8-x64-vs2019-with-vcruntime.zip",
+  "groonga-10.0.8-x64-vs2017.zip",
+  "groonga-10.0.8-x64-vs2019.zip",
+  "groonga-10.0.8-x64.zip",
+
+  "groonga-10.0.8-x86-vs2017-with-vcruntime.zip",
+  "groonga-10.0.8-x86-vs2019-with-vcruntime.zip",
+  "groonga-10.0.8-x86-vs2017.zip",
+  "groonga-10.0.8-x86-vs2019.zip",
+  "groonga-10.0.8-x86.zip",
+
+  "groonga-10.0.9-x64-vs2017-with-vcruntime.zip",
+  "groonga-10.0.9-x64-vs2019-with-vcruntime.zip",
+  "groonga-10.0.9-x64-vs2017.zip",
+  "groonga-10.0.9-x64-vs2019.zip",
+  "groonga-10.0.9-x64.zip",
+
+  "groonga-10.0.9-x86-vs2017-with-vcruntime.zip",
+  "groonga-10.0.9-x86-vs2019-with-vcruntime.zip",
+  "groonga-10.0.9-x86-vs2017.zip",
+  "groonga-10.0.9-x86-vs2019.zip",
+  "groonga-10.0.9-x86.zip",
+
+  "groonga-10.0.10-x64-vs2017-with-vcruntime.zip",
+  "groonga-10.0.10-x64-vs2019-with-vcruntime.zip",
+  "groonga-10.0.10-x64-vs2017.zip",
+  "groonga-10.0.10-x64-vs2019.zip",
+  "groonga-10.0.10-x64.zip",
+
+  "groonga-10.0.10-x86-vs2017-with-vcruntime.zip",
+  "groonga-10.0.10-x86-vs2019-with-vcruntime.zip",
+  "groonga-10.0.10-x86-vs2017.zip",
+  "groonga-10.0.10-x86-vs2019.zip",
+  "groonga-10.0.10-x86.zip",
+
+  "groonga-10.0.11-x64-vs2017-with-vcruntime.zip",
+  "groonga-10.0.11-x64-vs2019-with-vcruntime.zip",
+  "groonga-10.0.11-x64-vs2017.zip",
+  "groonga-10.0.11-x64-vs2019.zip",
+  "groonga-10.0.11-x64.zip",
+
+  "groonga-10.0.11-x86-vs2017-with-vcruntime.zip",
+  "groonga-10.0.11-x86-vs2019-with-vcruntime.zip",
+  "groonga-10.0.11-x86-vs2017.zip",
+  "groonga-10.0.11-x86-vs2019.zip",
+  "groonga-10.0.11-x86.zip"
+]
+
+base_url = "http://packages.groonga.org/windows/groonga"
+
+download_packages.each do |package|
+  dirname = File.basename(package, ".zip")
+
+  download_url = "#{base_url}/#{package}"
+  sh("wget", download_url)
+  sh("cp", package, "#{package}.org")
+  unzip_success = true
+  sh("unzip", "-q", package)
+end
